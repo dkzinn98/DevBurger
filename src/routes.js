f8e60286d3c6f47a33c6a4781f3e29389d0004c1
dkzinn98 <derykalcinodasilva@icloud.com>
2025-02-07 13:54:00 -0300

    yup-add-commit

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,10 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
+import UserController from './app/controllers/UserController';
 
-import User from './app/models/User.js';
 
 const routes = new Router()
 
-routes.get('/', async (request, response) => {
-    const user = await User.create({
-        id: v4(),
-        name: 'Deryk2',
-        email: 'deryk2@email.com',
-        password_hash: '@dmin!Dk'
-    });
+routes.post('/users', UserController.store);
+    
 
-    return response.status(201).json(user);
-})
-
-export default routes
+export default routes 
