dfbe9bf8352c0c39046b8e5ee8a53add65655abd
dkzinn98 <derykalcinodasilva@icloud.com>
2025-02-12 12:51:41 -0300

    config-tokenJWT-part2-commit

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer.js';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductController from './app/controllers/ProductController';
+import authMiddleware from './middlewares/auth.js';
+
 
 
 const routes = new Router();
@@ -11,7 +13,13 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware); // passando o authMiddleware para todas as rotas que tiver abaixo!
+
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
 
 export default routes 
+
+
+// recebemos request -> middleware (criar barreiras de autenticação) -> vamos para o controller -> acessamos a modelo -> vamos para o database -> responde = damos a resposta ao usuário
