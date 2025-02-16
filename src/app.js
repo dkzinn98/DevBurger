0dd24dd0604d8b1caef381da40647b0dc536831f
dkzinn98 <derykalcinodasilva@icloud.com>
2025-02-15 21:34:16 -0300

    features-finished-commit

--- src/app.js
+++ src/app.js
@@ -14,7 +14,13 @@ class App {
 
     middlewares() {
         this.app.use(express.json());
-        this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')));
+        this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')),
+    );
+
+    this.app.use(express.json());
+        this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads')),
+    );
+    
     }
 
 
