7649b18f21d5fcf0fd349bfb9e16450cf921d854
dkzinn98 <derykalcinodasilva@icloud.com>
2025-02-25 15:04:46 -0300

    debbug-commit

--- frontend/src/containers/Login/index.jsx
+++ frontend/src/containers/Login/index.jsx
@@ -1,82 +1,101 @@
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 import * as yup from "yup";
-import { toast } from 'react-toastify'
-import { api } from '../../services/api.js'
+import { api } from "../../services/api.js";
 
-import Logo from '../../assets/logo-devburger-login.svg';
+import Logo from "../../assets/logo-devburger-login.svg";
 
-import { 
-    Container,
-    Form,
-    InputContainer,
-    LeftContainer,
-    RightContainer,
-    Title
+import {
+	Container,
+	Form,
+	InputContainer,
+	LeftContainer,
+	RightContainer,
+	Title,
 } from "./styles.js";
 
-import { Button } from '../components/Button';
+import { Button } from "../components/Button";
 
-export function Login(){
-    
-    const schema = yup.object({
-        email: yup.string().email('Digite um e-mail válido!').required('O e-mail é obrigatório!'),
-        password: yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('Digite uma senha!'),
-      }).required();
+export function Login() {
+	const schema = yup
+		.object({
+			email: yup
+				.string()
+				.email("Digite um e-mail válido!")
+				.required("O e-mail é obrigatório!"),
+			password: yup
+				.string()
+				.min(6, "A senha deve ter pelo menos 6 caracteres")
+				.required("Digite uma senha!"),
+		})
+		.required();
 
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
 
-      const { register, handleSubmit, formState:{ errors } } = useForm({
-        resolver: yupResolver(schema)
-      });
-      
-      console.log(errors);
-      
-      const onSubmit = async (data) => {
-        const response = await toast.promise(
-            api.post('/sessions', {
-                email: data.email,
-                password: data.password
-        }),
-        {
-            pending: 'Verificando seus dados',
-            success: 'Seja Bem-Vindo(a) 👌',
-            error: 'Email ou senha incorretos 🤯'
-        },
-    );
-        
-    console.log(response);
+	console.log(errors);
 
-      }
+	const onSubmit = async (data) => {
+		const response = await toast.promise(
+			api.post("/sessions", {
+				email: data.email,
+				password: data.password,
+			}),
+			{
+				pending: "Verificando seus dados",
+				success: "Seja Bem-Vindo(a) 👌",
+				error: "Email ou senha incorretos 🤯",
+			},
+		);
 
-    return (
-        <Container>
-            <LeftContainer>
-                <img src={Logo} alt='logo-devburger' />
-            </LeftContainer>
-            <RightContainer>
-                <Title>
-                Olá, seja bem-vindo ao <span>Dev Burguer</span>!
-                <br />
-                Acesse com seu <span>Login e senha</span>.
-                </Title>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <InputContainer>
-                        <label>Email</label>
-                        <input type="email" {...register("email")} placeholder="Digite seu email" />
-                        <p>{errors?.email?.message}</p>
-                    </InputContainer>
+		console.log(response);
+	};
 
-                    <InputContainer>
-                        <label>Senha</label>
-                        <input type="password" {...register("password")} placeholder="Digite sua senha" />
-                        <p>{errors?.password?.message}</p>
+	return (
+		<Container>
+			<LeftContainer>
+				<img src={Logo} alt="logo-devburger" />
+			</LeftContainer>
+			<RightContainer>
+				<Title>
+					Olá, seja bem-vindo ao <span>Dev Burguer</span>!
+					<br />
+					Acesse com seu <span>Login e senha</span>.
+				</Title>
+				<Form onSubmit={handleSubmit(onSubmit)}>
+					<InputContainer>
+						{/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
+						<label>Email</label>
+						<input
+							type="email"
+							{...register("email")}
+							placeholder="Digite seu email"
+						/>
+						<p>{errors?.email?.message}</p>
+					</InputContainer>
 
-                    </InputContainer>
-                    <Button type="submit">Entrar</Button>
-                </Form>
-                <p>Não possui conta? <a href='./cadastro'>Clique aqui!</a>
-                </p>
-            </RightContainer>
-        </Container>
-    )
+					<InputContainer>
+						{/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
+						<label>Senha</label>
+						<input
+							type="password"
+							{...register("password")}
+							placeholder="Digite sua senha"
+						/>
+						<p>{errors?.password?.message}</p>
+					</InputContainer>
+					<Button type="submit">Entrar</Button>
+				</Form>
+				<p>
+					Não possui conta? <a href="./cadastro">Clique aqui!</a>
+				</p>
+			</RightContainer>
+		</Container>
+	);
 }
