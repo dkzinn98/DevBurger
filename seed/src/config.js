3699caa40a64998f5b6ae631577a45607ce64c62
dkzinn98 <derykalcinodasilva@icloud.com>
2025-02-27 17:46:15 -0300

    seed-test

---
+++ seed/src/config.js
@@ -0,0 +1,5 @@
+export const config = {
+	userToken:
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImJiNzlkOWVjLTFmZGItNGMwZC05ODkyLWI0OGJkOTFiZjk4NiIsIm5hbWUiOiJ0ZXN0ZSIsImlhdCI6MTc0MDY4NDc2NywiZXhwIjoxNzQwNzcxMTY3fQ.7VD1UaRsTo3dRFZkklN13znUJVzOPgMBDxV89LcEwU4",
+	apiUrl: "http://localhost:3001",
+};
